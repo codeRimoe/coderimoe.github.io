e8546104a48799949b8015e306972831133a1c93
unarimoe <negatronljl@gmail.com>
2017-06-02 04:16:30 +0800

    game kanna

--- _site/assets/js/game/kanna/obj_K.js
+++ _site/assets/js/game/kanna/obj_K.js
@@ -1,5 +1,4 @@
 //obj K
-
 var K = function (image,x,y,hp,spd,boom,st){
     this.img = image[0];
     this.x = x;
@@ -8,19 +7,40 @@ var K = function (image,x,y,hp,spd,boom,st){
     this.spd = spd;
     this.boom = boom;
     this.st=st;
+    this.kill=[0,0,0,0];
+    this.sum=0;
 };
 
 K.prototype.draw = function (ctx) {
-    ctx.drawImage(this.img, this.x-32, this.y-32, 32 ,32);
+    ctx.drawImage(this.img, this.x-16, this.y-16, 32 ,32);
 };
 
 K.prototype.move = function (ctx) {
+       //stop [ud,spdown,rew,dizz,]
+    this.sum=0;
+    this.spd = initspd;
+    if(this.st[0])
+        this.sum++;
+    if(this.st[1]) {
+        this.spd = initspd - 3 ;
+        this.sum ++;
+    }
+    if(this.st[2]){
+        this.spd *= -1 ;
+        this.sum ++;
+    }
+    if(this.st[3]){
+        this.spd = 0 ;
+        this.sum ++;
+    }
     this.y += (38 in keysDown)*(-this.spd)+(40 in keysDown)*this.spd;
     this.x += (37 in keysDown)*(-this.spd)+(39 in keysDown)*this.spd;
     if(this.y<=48)this.y=48;
-    if(this.y>=CBH-32)this.y=CBH-32;
-    if(this.x<=60)this.x=60;
-    if(this.x>=CBW-20)this.x=CBW-20;
+    if(this.y>=CBH-48)this.y=CBH-48;
+    if(this.x<=48)this.x=48;
+    if(this.x>=CBW-48)this.x=CBW-48;
+    if(kanna.sum>0)kanna.img=kimg[1];
+    else kanna.img=kimg[0];
     this.draw(ctx);
 };
 
@@ -29,7 +49,7 @@ K.prototype.setB = function (B) {
         if(B[i].hp<=0){
             B[i].x = this.x;
             B[i].y = this.y;
-            B[i].hp = 100;
+            B[i].hp = bexptime;
             break;
         }
     }
@@ -37,9 +57,11 @@ K.prototype.setB = function (B) {
 
 K.prototype.stCheck = function(){
     for(i=0;i<this.st.length;i++){
-        if(this.st[i]>0)
+        if(this.st[i]>0){
             this.st[i]-=1;
-        else
+        }
+        else{
             this.st[i]=0;
+        }
     }
 }
--- _site/assets/js/game/kanna/setting.js
+++ _site/assets/js/game/kanna/setting.js
@@ -4,8 +4,10 @@ var mainctx = main.getContext("2d");
 var state = document.getElementById("stateCanvas");
 var statectx = state.getContext("2d");
 
-var CBH=600,CBW=1080
-var ttt=0,ppp=1,mmm=0,hhh=0,los=1;
+var CBH=600,CBW=1080,bexptime=150,maxwt=300,wk=50;
+var inithp=10,initspd=5,initboom=8,initzz=5;
+var inithp_M=5,initspd_M=3;
+var ttt=0,ppp=1,mmm=0,hhh=0,los=1,wt=maxwt,boomnum=initboom,allkill=0;
 
 var reftime=12;
 var boomcd=200;
@@ -22,24 +24,23 @@ addEventListener("keyup", function (e) {
                  delete keysDown[e.keyCode];
                  switch (e.keyCode){
                  case 13:if(mmm&&los){ppp=pause(mainctx,statectx,ppp);}else if(!mmm){mmm=1;}break;
-                 case 27:if((!ppp)||(!los))reset(),menu(mainctx,statectx);break;
+                 case 27:if((!ppp)||(!los)){reset(),menu(mainctx,statectx);}break;
                  case 72:if(!mmm){hhh++,hhh%=2,menu(mainctx,statectx);}break;
                  }
                  }, false);
 
-touch = function (A,B){
+touch = function (A,B,dis){
     var dx = Math.abs(A.x-B.x);
     var dy = Math.abs(A.y-B.y);
-    if(Math.max(dx,dy)<32)
+    if(Math.max(dx,dy)<dis)
         return true;
     return false;
 }
 
 pause = function(mainctx,statectx,ppp){
     if(ppp){
-        mainctx.fillStyle="rgba(255,255,255,0.5)";
-        mainctx.fillRect(0,0,CBW,CBH);
-        statectx.clearRect(0, 0, 1200, 100);
+        mainctx.drawImage(pausepic, 0, 0);
+        statectx.clearRect(0, 0, CBW, 100);
         statectx.drawImage(pbg[0], 0, 0);
         return 0;
     }
@@ -47,24 +48,31 @@ pause = function(mainctx,statectx,ppp){
 }
 
 lose = function(mainctx,statectx){
-    alert("You Lose!Game Over!");
-    mainctx.fillStyle="rgba(0,0,0,0.5)";
-    mainctx.fillRect(0,0,CBW,CBH);
+    alert("Game Over! You Lose!\nKill:"+allkill+"   Time:"+(maxwt-wt));
     mainctx.drawImage(losepic, 0, 0);
-    statectx.clearRect(0, 0, 1200, 100);
+    statectx.drawImage(pbg[1], 0, 0);
+    los=0;
+}
+
+win = function(mainctx,statectx){
+
+    alert("Congratulations! You Win!\nKill:"+allkill+"   Time:"+(maxwt-wt));
+    mainctx.drawImage(winpic, 0, 0);
     statectx.drawImage(pbg[1], 0, 0);
     los=0;
 }
 
 menu = function(mainctx,statectx){
     mainctx.drawImage(sbgmain[hhh], 0, 0);
-    statectx.clearRect(0, 0, 1200, 100);
+    statectx.clearRect(0, 0, CBW, 100);
     statectx.drawImage(sbgst[0], 0, 0);
 }
 
 reset = function(){
-    ppp=1,mmm=0,hhh=0,los=1;
-    kanna = new K(kimg,CBW/2,CBH/2,10,5,10,[0,0,0,0,0]);
+    keysDown={};
+    ttt=0,ppp=1,mmm=0,hhh=0,los=1,wt=maxwt,boomnum=initboom,allkill=0;
+    kanna = new K(kimg,CBW/2-128,CBH/2-128,inithp,initspd,initboom,[0,0,0,0],0);
+    kobayashi = new X(ling,5,0);
     baka_pool = [];
     boom_pool = [];
     for(i = 0; i < kanna.boom;i++)
@@ -75,7 +83,10 @@ var kimg = [new Image(),new Image()];
 var ming = [new Image(),new Image(),new Image(),new Image()];
 var bing = [new Image(),new Image(),new Image(),new Image(),new Image(),new Image()];
 var ling = [new Image(),new Image()];
+var stpic =[new Image(),new Image(),new Image(),new Image(),new Image(),new Image(),new Image(),new Image()];
 var losepic=new Image();
+var winpic=new Image();
+var pausepic=new Image();
 var sbgmain = [new Image(),new Image()];
 var sbgst = [new Image(),new Image()];
 var pbg=[new Image(),new Image()];
@@ -96,7 +107,17 @@ bing[2].src = "/assets/img/game/kanna/boom/boom3.png";
 bing[3].src = "/assets/img/game/kanna/boom/boom4.png";
 bing[4].src = "/assets/img/game/kanna/boom/boom5.png";
 bing[5].src = "/assets/img/game/kanna/boom/boom6.png";
+stpic[0].src = "/assets/img/game/kanna/state/udoff.png";
+stpic[1].src = "/assets/img/game/kanna/state/sdoff.png";
+stpic[2].src = "/assets/img/game/kanna/state/disoff.png";
+stpic[3].src = "/assets/img/game/kanna/state/dzoff.png";
+stpic[4].src = "/assets/img/game/kanna/state/udon.png";
+stpic[5].src = "/assets/img/game/kanna/state/sdon.png";
+stpic[6].src = "/assets/img/game/kanna/state/dison.png";
+stpic[7].src = "/assets/img/game/kanna/state/dzon.png";
 losepic.src = "/assets/img/game/kanna/lose.png";
+winpic.src = "/assets/img/game/kanna/win.png";
+pausepic.src = "/assets/img/game/kanna/pause.png";
 stbg.src = "/assets/img/game/kanna/bg.png";
 pbg[0].src = "/assets/img/game/kanna/pbg.png";
 pbg[1].src = "/assets/img/game/kanna/ebg.png";
@@ -105,4 +126,3 @@ sbgmain[1].src = "/assets/img/game/kanna/help.png";
 sbgst[0].src = "/assets/img/game/kanna/sbg.png";
 sbgst[1].src = "/assets/img/game/kanna/hbg.png";
 bg.src = "/assets/img/game/kanna/background.png";
-
