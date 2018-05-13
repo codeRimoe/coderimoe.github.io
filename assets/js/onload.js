2de19bb2119f26c8640de21fdb1b15383118ae1a
Yue Luo <negatron@gmail.com>
2018-05-14 00:40:00 +0800

    repair_sponsor

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+window.onload = function(){
     //xml files
     if (window.XMLHttpRequest){
         // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -43,8 +43,55 @@ window.onload=function(){
         }
         _ctx += "</li>";
         ctx += _ctx;
-        console.log(_ctx);
     }
-    console.log(x);
     prj_show.innerHTML=ctx;
+
+    //Sponsor
+    var shareArea = document.getElementById('share');
+    var back = document.getElementById('back');
+    var ali = document.getElementById('ali');
+    var wc = document.getElementById('wechat');
+    var bb = document.getElementById('bb');
+    var alp = document.getElementById('alp');
+    var wcp = document.getElementById('wcp');
+    var lop = document.getElementById('lop');
+    var loco = document.getElementById('loco');
+    var xx = document.getElementById('xx');
+    bb.style.display = 'none';
+    xx.style.display = 'none';
+
+    var click=0;
+
+    shareArea.onclick = function(){
+        bb.style.display = 'block';
+        shareArea.style.display = 'none';
+    };
+    back.onclick = function(){
+        bb.style.display = 'none';
+        shareArea.style.display = 'block';
+        loco.src=lop.src;
+    };
+
+    ali.onclick = function(){
+        if(click==1){
+            loco.src=lop.src;
+            click=0;
+        }
+        else{
+            loco.src=alp.src;
+            click=1;
+        }
+    };
+
+    wc.onclick = function(){
+        if(click==2){
+            loco.src=lop.src;
+            click=0;
+        }
+        else{
+            loco.src=wcp.src;
+            click=2;
+        }
+    };
+
 }
